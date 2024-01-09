a4a5261dcddeacb80bd4a3e7e042b02dd6dfdfe7
leandromoren <leandro.moren@outlook.com>
2024-01-09 20:20:19 -0300

    xx

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movies'
-ROOT = 'https://subslikescript.com/'
-
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 resultado = requests.get(website)
-
 contenido = resultado.text
-
 soup = BeautifulSoup(contenido, 'lxml')
 # print(soup.prettify())
+box = soup.find('article', class_='main-article')
 
 # Lista de todas las peliculas
-allMoviesBox = soup.find('article', class_='main-article')
-
 links = []
-for link in allMoviesBox.find_all('a', href=True):
-    links.append(ROOT + link['href'])
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
 
-print(links)
+for link in links:
+    website = f'{root}/{link}'
+    resultado = requests.get(website)
+    contenido = resultado.text
+    soup = BeautifulSoup(contenido, 'lxml')
 
-# box = soup.find('article', class_='main-article')
-# titulo = box.find('h1').get_text()
-# transcripcion = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+    box = soup.find('article', class_='main-article')
 
-# with open(f'{titulo}.txt', 'w', encoding='utf-8') as file:
-#     file.write(transcripcion)
+    titulo = box.find('h1').get_text()
+    transcripcion = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+    with open(f'{titulo}.txt', 'w', encoding='utf-8') as file:
+        file.write(transcripcion)
