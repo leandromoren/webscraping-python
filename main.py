5d51aab6a39d7763216524471960785a8179d8d1
leandromoren <leandro.moren@outlook.com>
2024-01-09 19:39:48 -0300

    xx

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/Titanic-120338'
+website = 'https://subslikescript.com/movies'
+ROOT = 'https://subslikescript.com/'
 
 resultado = requests.get(website)
 
 contenido = resultado.text
 
 soup = BeautifulSoup(contenido, 'lxml')
-print(soup.prettify())
+# print(soup.prettify())
 
-box = soup.find('article', class_='main-article')
-titulo = box.find('h1').get_text()
-transcripcion = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+# Lista de todas las peliculas
+allMoviesBox = soup.find('article', class_='main-article')
 
-with open(f'{titulo}.txt', 'w', encoding='utf-8') as file:
-    file.write(transcripcion)
+links = []
+for link in allMoviesBox.find_all('a', href=True):
+    links.append(ROOT + link['href'])
+
+print(links)
+
+# box = soup.find('article', class_='main-article')
+# titulo = box.find('h1').get_text()
+# transcripcion = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+# with open(f'{titulo}.txt', 'w', encoding='utf-8') as file:
+#     file.write(transcripcion)
